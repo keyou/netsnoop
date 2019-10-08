5472fc27ad720c6f95d993ec9f43d81a986a46a8
keyou <keyou@cvte.com>
2019-10-08 10:12:22 +0800

    fix delay stat error

--- net_snoop_server.cc
+++ net_snoop_server.cc
@@ -386,7 +386,7 @@ int NetSnoopServer::ProcessNextCommand()
                 netstat_->send_avg_speed /= *peers_active;
                 netstat_->recv_avg_speed /= success_count;
                 netstat_->recv_time /= success_count;
-                netstat_->delay /= *peers_active;
+                netstat_->delay /= success_count;
                 if (command->is_multicast)
                 {
                     netstat_->loss = 1 - 1.0 * netstat_->recv_bytes / (netstat_->send_bytes * success_count);
